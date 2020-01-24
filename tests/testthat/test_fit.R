eea9ff7a616bf4a462e87d10ceca0c58135afc88
David Lawrence Miller <github@ninepointeightone.net>
2020-01-24 20:28:42 +0000

    add more tests

--- tests/testthat/test_fit.R
+++ tests/testthat/test_fit.R
@@ -7,7 +7,7 @@ library(CTMCdive)
 
 load(system.file("simdat.RData", package="CTMCdive"))
 
-test_that("point estimates are correct",{
+test_that("surface~s(time), dive~s(time)",{
   forms <- list(surface ~ s(time, bs="cs"),
                 dive ~ s(time, bs="cs"))
 
@@ -18,6 +18,29 @@ test_that("point estimates are correct",{
   expect_equal(mod$res$dive$Est, -3.073709637)
 })
 
+
+test_that("surface~1, dive~s(time)",{
+  forms <- list(surface ~ 1,
+                dive ~ s(time, bs="cs"))
+
+  # fit model
+  mod <- FitCTMCdive(forms, simdat, print = FALSE)
+
+  expect_equal(mod$res$surface$Est, -2.90144483)
+  expect_equal(mod$res$dive$Est, -3.073735243)
+})
+
+
+test_that("surface~s(time), dive~1",{
+  forms <- list(surface ~ s(time, bs="cs"),
+                dive ~ 1)
+
+  # fit model
+  mod <- FitCTMCdive(forms, simdat, print = FALSE)
+
+  expect_equal(mod$res$surface$Est, -1.841374046)
+  expect_equal(mod$res$dive$Est, -3.073729866)
+})
 
 
 
