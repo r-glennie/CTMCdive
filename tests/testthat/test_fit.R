a9c88c6c34f4434370af20f77323086bd41271ee
David Lawrence Miller <github@ninepointeightone.net>
2020-01-24 12:40:42 +0000

    first test of point estimates

---
+++ tests/testthat/test_fit.R
@@ -0,0 +1,25 @@
+# test FitCTMCdive
+
+context("FitCTMCdive")
+
+# load data etc
+library(CTMCdive)
+
+load(system.file("inst/simdat.RData", package="CTMCdive"))
+
+test_that("point estimates are correct",{
+  forms <- list(surface ~ 1,
+                dive ~ 1)
+
+  # fit model
+  mod <- FitCTMCdive(forms, simdat, model = "sd", print = TRUE)
+
+  expect_equal(mod$res$surface$Est, -1.841388492)
+  expect_equal(mod$res$dive$Est, -3.073709637)
+})
+
+
+
+
+
+
